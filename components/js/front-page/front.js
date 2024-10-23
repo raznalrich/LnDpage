c9ad53c79a3ff0097e599006231e5dd68f692ced
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2024-10-23 16:44:40 +0530

    feat:calendar

--- components/js/front-page/front.js
+++ components/js/front-page/front.js
@@ -1,6 +1,10 @@
 let slideIndex = 0;
 let cycleCount = 0; 
 const maxCycles = 3; 
+import { storage,database} from "../calenderAPI";
+import { child, get, getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
+import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
+
 
 showSlides();
 
@@ -77,3 +81,61 @@ function currentSlide(n) {
 // window.onload = function(){
 //     rhombusoverlay();
 // }
+
+
+//Calendar fetching
+
+function showCalendarEvents(){
+    const dref = ref(database);
+    let div = document.getElementById('swiperCalendar');
+  
+    get(child(dref,'courses')).then((snapshot)=>{
+        if(snapshot.exists()){
+            snapshot.forEach((menu)=>{
+                let value = menu.val();
+                console.log(value);
+                
+                const courseName = value.courseName;
+                const startDate = value.startDate;
+                const startTime = value.startTime;
+                const endDate = value.endDate;
+                const endTime = value.endTime;
+                const keyPoints = value.keyPoints;
+                const maxParticipation = value.maxParticipation;
+                const targetAudience = value.targetAudience;
+                const trainerName = value.trainerName;
+                const mode = value.mode;
+  
+                const card = document.createElement('div');
+                card.classList.add('swiper-slide');
+                card.innerHTML = `
+        
+        <div class="calendarEventContainer">
+                    <div class="date">
+                        <p>Oct</p>
+                        <h2>26</h2>
+                        <p style="margin-top: 10px;font-size: 15px;">2024</p>
+                    </div>
+                    <img src="./components/assets/imga.png" alt="" srcset="">
+                    <div class="coursename">
+                        <p><strong>${courseName}</strong></p>
+                    </div>
+                </div>
+      `;
+  
+  
+      div.appendChild(card);
+                
+                
+            })
+        }
+        else{
+            let p = document.createElement('p');
+            p.innerHTML = 'No files founded';
+            div.appendChild(p);
+        }
+    })
+  
+  }
+  
+  showCalendarEvents();
