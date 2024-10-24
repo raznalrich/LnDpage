d534109fa1e27ed8e3976cbbf3c489d7caf65969
Shalu Murali <shalu.murali@experionglobal.local>
2024-10-24 14:59:48 +0530

    newsletter preview

---
+++ components/js/front-page/newsletter.js
@@ -0,0 +1,3 @@
+document.getElementById('newsletter-button').onclick = () => {
+    window.location.href = 'https://firebasestorage.googleapis.com/v0/b/lndvconnect-6f4ac.appspot.com/o/newsletter%2Fmyfile%20(4)%201%202.html?alt=media&token=78bd5803-7f52-452d-ab0d-42b3926744d7'
+}
