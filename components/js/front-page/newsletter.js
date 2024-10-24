24ba470434ddaeab7f125c71d63b62a246b40e7d
Shalu Murali <shalu.murali@experionglobal.local>
2024-10-24 19:08:28 +0530

    newsletter final commit

--- components/js/front-page/newsletter.js
+++ components/js/front-page/newsletter.js
@@ -1,3 +1,21 @@
-document.getElementById('newsletter-button').onclick = () => {
-    window.location.href = 'https://firebasestorage.googleapis.com/v0/b/lndvconnect-6f4ac.appspot.com/o/newsletter%2Fmyfile%20(4)%201%202.html?alt=media&token=78bd5803-7f52-452d-ab0d-42b3926744d7'
-}
+import { storage, database } from "../Firebase.js";
+import { child, get, getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
+
+const dref = ref(getDatabase(), 'newsletter');
+get(dref).then((snapshot) => {
+    if (snapshot.exists()) {
+        const data = snapshot.val();
+        console.log(data[1]);
+        document.getElementById('newsletter-iframe').src = data[1].htmlurl
+        document.getElementById('newsletter-button').onclick = () => {
+            window.location.href = data[1].htmlurl
+        }
+    } else {
+        console.log("No data available");
+    }
+}).catch((error) => {
+    console.error("Error fetching data: ", error);
+});
+
+
+
