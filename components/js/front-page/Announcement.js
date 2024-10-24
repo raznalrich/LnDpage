151f48abdef984f46300acba4ee25131d194bedb
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-10-24 14:16:33 +0530

    Announcement section

--- components/js/front-page/Announcement.js
+++ components/js/front-page/Announcement.js
@@ -9,7 +9,7 @@ import {database,child, get,ref} from "../Firebase.js";
         let announcementDesc = announcement.child('desc').val();
         let announcementIndex = announcement.child('index').val();
         let announcementTitle = announcement.child('title').val();
-        console.log(announcementTitle);
+        console.log(announcementDesc);
         
         
             var item = `<div class="announcement-item">
