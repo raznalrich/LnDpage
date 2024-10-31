504161174db0b2a766eb520aa6169ce894406994
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2024-10-31 22:14:24 +0530

    fix banner

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { storage, database, app } from "../LnDpage/components/js/Firebase.js";
+import { storage, database, app } from "./components/js/Firebase.js";
 import { child, get, getDatabase,query, set, ref as dbRef } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
 import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
@@ -131,3 +131,20 @@ announcementContainer.addEventListener('mouseleave', removeStayedNotification);
     })
 }
 document.addEventListener("DOMContentLoaded",addNotifications())
+
+
+const databaseURL = "https://training-calendar-ilp05-default-rtdb.asia-southeast1.firebasedatabase.app/courses/.json";
+
+fetch(databaseURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Logs the data from the Firebase database
+  })
+  .catch(error => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
